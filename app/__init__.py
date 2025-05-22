da41e2a05c3c928af8dff1312628eddda584439e
Shivam Maheshwari <shivammaheshwari317@gmail.com>
2025-05-22 08:31:16 +0530

    Regiter restaurant blueprint and migration settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from app.config import Config
 from app.extensions import db, ma, jwt
+from flask_migrate import Migrate
 from app.routes.auth_routes import auth_bp
+from app.routes.restaurant_routes import restaurant_bp
+from app.extensions import db, ma, jwt, migrate
 
 def create_app():
     app = Flask(__name__)
@@ -10,8 +13,10 @@ def create_app():
     db.init_app(app)
     ma.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(auth_bp)
+    app.register_blueprint(restaurant_bp)
 
     with app.app_context():
         db.create_all()
