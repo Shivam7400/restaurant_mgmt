bd8dab70cec28cc4b6748b5e54a68b6a26a68264
Shivam Maheshwari <shivammaheshwari317@gmail.com>
2025-06-03 00:49:40 +0530

    register the category blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.routes.auth_routes import auth_bp
 from app.routes.restaurant_routes import restaurant_bp
 from app.routes.branch_routes import branch_bp
 from app.routes.menu_routes import menu_bp
+from app.routes.category_item_routes import category_bp
 from app.extensions import db, ma, jwt, migrate
 
 def create_app():
@@ -21,6 +22,7 @@ def create_app():
     app.register_blueprint(restaurant_bp)
     app.register_blueprint(branch_bp)
     app.register_blueprint(menu_bp)
+    app.register_blueprint(category_bp)
 
     with app.app_context():
         db.create_all()
