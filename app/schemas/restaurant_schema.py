8f1642a424c844fbd477a0ea77daf6d61c0b5921
Shivam Maheshwari <shivammaheshwari317@gmail.com>
2025-07-01 07:27:02 +0530

    updated fields in schema

--- app/schemas/restaurant_schema.py
+++ app/schemas/restaurant_schema.py
@@ -4,5 +4,7 @@ from app.schemas.branch_schema import BranchSchema
 class RestaurantSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+    location = fields.Str(required=True)
+    contact_number = fields.Str(required=True)
     description = fields.Str()
     branches = fields.List(fields.Nested(BranchSchema), dump_only=True)
