4b46cf4b073727a88da5c4524e0c77daa621dd78
Shivam Maheshwari <shivammaheshwari317@gmail.com>
2025-07-01 07:26:12 +0530

    added fields in restaurant model

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -5,6 +5,8 @@ class Restaurant(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False, unique=True)
+    location = db.Column(db.String(150), nullable=False)
+    contact_number = db.Column(db.String(20), nullable=False)
     description = db.Column(db.Text)
     branches = db.relationship("Branch", backref="restaurant", cascade="all, delete-orphan")
 
